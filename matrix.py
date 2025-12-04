88c6aa5118f7e3fa4bf6e501bdea23eda5df456b
yan <@>
2025-12-04 18:29:00 +0100

    2025 Day 4

---
+++ matrix.py
@@ -0,0 +1,73 @@
+import os
+import copy
+class Matrix:
+
+    def __init__(self, data: list[list[object] | str] = [], height: int = 0, width: int = 0, default:object = 0):
+        if data == [] and (height <= 0 or width <= 0):
+            raise Exception()
+
+        if data != []:
+            if isinstance(data[0], str):
+                self.data = [list(l) for l in data]
+            else:
+                self.data = data
+            self.height = len(data)
+            self.width = len(data[0])
+        else:
+            self.data = []
+            self.height = height
+            self.width = width
+            for _ in range(height):
+                self.data.append([default for _ in range(width)])
+
+    def around(self, h: int, w: int, search:object = None, center: bool = False, none: bool = False, oob: bool = False) -> tuple[list[object], int] | None:
+        if not self.valid(h, w) and not oob:
+            return None
+        res = []
+        for hi in [h-1, h, h+1]:
+            for wi in [w-1, w, w+1]:
+                if not center and hi == h and wi == w:
+                    continue
+                val = self.g(hi, wi)
+                if val is not None or none:
+                    res.append(val)
+        cnt = len(res) if not search else len([i for i in res if i == search])
+        return res, cnt
+
+    def g(self, h: int, w: int) -> object|None:
+        if self.valid(h, w):
+            return self.data[h][w]
+        return None
+
+    def s(self, h: int, w: int, val) -> bool:
+        if self.valid(h, w):
+            self.data[h][w] = val
+            return True
+        return False
+
+    def valid(self, h: int, w: int):
+        return h >= 0 and h < self.height and w >= 0 and w < self.width
+
+    def copy(self):
+        return Matrix(data=copy.deepcopy(self.data))
+
+    def __str__(self, align: bool = True):
+        min_size = 1
+        if align:
+            for i in range(self.height):
+                for j in range(self.width):
+                    min_size = max(min_size, len(str(self.data[i][j])))
+        str_ = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                val = str(self.data[i][j])
+                str_ += val.rjust(min_size + 1)
+            str_ += os.linesep
+        return str_
+
+
+if __name__ == "__main__":
+    m = Matrix(height=3, width=4)
+    m.s(2,1, 1000)
+    print(m)
+    print(m.around(-1,1, none = True, include=True, oob = True))
--- y2025/d04.py
+++ y2025/d04.py
@@ -1,8 +1,33 @@
 import day
+from matrix import Matrix
 
 class Day(day.Day):
     def p1(self):
-        pass
+        m = Matrix(self.lines)
+        s = 0
+        for h in range(m.height):
+            for w in range(m.width):
+                if m.g(h,w) == "@":
+                    if m.around(h,w, search="@") < 4:
+                        s += 1
+        return s
+
+
 
     def p2(self):
-        pass
+        m_curr = Matrix(self.lines)
+        m_next = m_curr.copy()
+        s = 1
+        total = 0
+        while s != 0:
+            s = 0
+            for h in range(m_curr.height):
+                for w in range(m_curr.width):
+                    if m_curr.g(h,w) == "@":
+                        if m_curr.around(h,w, search="@") < 4:
+                            s += 1
+                            m_next.s(h,w,".")
+            m_curr = m_next
+            m_next = m_curr.copy()
+            total += s
+        return total
